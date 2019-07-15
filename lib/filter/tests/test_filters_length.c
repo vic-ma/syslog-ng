35c76b906f275cc1d2d2b03b4518372e67d4cccc
Victor Ma <vic.ma@mail.utoronto.ca>
2019-07-14 23:53:48 -0400

    Finish test base
    Signed-off-by: Victor Ma <vic.ma@mail.utoronto.ca>

---
+++ lib/filter/tests/test_filters_length.c
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2019 Victor Ma
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#include "filter/filter-expr.h"
+#include "cfg.h"
+#include "test_filters_common.h"
+
+#include <criterion/criterion.h>
+#include <criterion/parameterized.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+TestSuite(filter, .init = setup, .fini = teardown);
+
+typedef struct _FilterParamLength
+{
+  const gchar *msg;
+  int length;
+  gboolean expected_result;
+} FilterParamLength;
+
+ParameterizedTestParameters(filter, test_filter_length)
+{
+  static FilterParamLength test_data_list[] =
+    {
+        {.msg = "", .length = 0, .expected_result = TRUE},
+        {.msg = "abc", .length = 3, .expected_result = TRUE},
+        {.msg = "abc", .length = 4, .expected_result = FALSE},
+        {.msg = "", .length = , .expected_result = },
+        {.msg = "", .length = , .expected_result = },
+        {.msg = "", .length = , .expected_result = },
+    };
+
+  return cr_make_param_array(FilterParamLength, test_data_list,  G_N_ELEMENTS(test_data_list));
+}
+
+ParamterizedTest(FilterParamLength *param, filter, test_filter_length);
