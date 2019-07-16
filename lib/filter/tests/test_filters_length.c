d950810fc2c8a66ed1ca84f6862661ab451b9144
Victor Ma <vic.ma@mail.utoronto.ca>
2019-07-16 00:48:04 -0400

    Finish tests
    Signed-off-by: Victor Ma <vic.ma@mail.utoronto.ca>

--- lib/filter/tests/test_filters_length.c
+++ lib/filter/tests/test_filters_length.c
@@ -21,6 +21,7 @@
  */
 
 #include "filter/filter-expr.h"
+#include "filter/filter-length.h"
 #include "cfg.h"
 #include "test_filters_common.h"
 
@@ -43,15 +44,18 @@ ParameterizedTestParameters(filter, test_filter_length)
 {
   static FilterParamLength test_data_list[] =
     {
-        {.msg = "", .length = 0, .expected_result = TRUE},
-        {.msg = "abc", .length = 3, .expected_result = TRUE},
-        {.msg = "abc", .length = 4, .expected_result = FALSE},
-        {.msg = "", .length = , .expected_result = },
-        {.msg = "", .length = , .expected_result = },
-        {.msg = "", .length = , .expected_result = },
+        {.msg = "",                                                 .length = 0,  .expected_result = TRUE },
+        {.msg = "<15> openvpn[2499]: ",                             .length = 0,  .expected_result = TRUE },
+        {.msg = "<15> openvpn[2499]: PTHREAD support initialized",  .length = 27, .expected_result = TRUE },
+        {.msg = "<15> openvpn[2499]: PTHREAD support initialized",  .length = 26, .expected_result = FALSE},
+        {.msg = "<15> openvpn[2499]: PTHREAD support initialized",  .length = 0,  .expected_result = FALSE},
     };
 
   return cr_make_param_array(FilterParamLength, test_data_list,  G_N_ELEMENTS(test_data_list));
 }
 
-ParamterizedTest(FilterParamLength *param, filter, test_filter_length);
+ParameterizedTest(FilterParamLength *param, filter, test_filter_length)
+{
+  FilterExprNode *filter = filter_length_new(param->length);
+  testcase(param->msg, filter, param->expected_result);
+}
